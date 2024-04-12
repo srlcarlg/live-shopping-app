7a2d85dcba613f1a9cfece6dfe5276dd3f76b365
srlcarlg <carlgsr1@gmail.com>
2024-04-12 14:46:31 -0300

    quarkus create app via Eclipse IDE

---
+++ src/test/java/live/manager/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package live.manager;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
