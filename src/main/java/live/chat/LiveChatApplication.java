932d3afeb7015d83c1f00789bead618381836d9b
srlcarlg <carlgsr1@gmail.com>
2024-04-17 09:21:20 -0300

    spring create app via Eclipse IDE

---
+++ src/main/java/live/chat/LiveChatApplication.java
@@ -0,0 +1,13 @@
+package live.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveChatApplication.class, args);
+	}
+
+}
