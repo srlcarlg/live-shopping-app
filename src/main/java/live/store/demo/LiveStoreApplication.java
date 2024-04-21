1d21f224227d3d4a3a73ca28308dad4ae2263bae
srlcarlg <carlgsr1@gmail.com>
2024-04-20 21:21:30 -0300

    spring create app via Eclipse IDE

---
+++ src/main/java/live/store/demo/LiveStoreApplication.java
@@ -0,0 +1,13 @@
+package live.store.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/store/demo/LiveStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package live.store.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
