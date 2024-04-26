fc134c1f5a169566a164e06d03e79d9ab35ea903
srlcarlg <carlgsr1@gmail.com>
2024-04-26 08:14:05 -0300

    feat: add latest transaction query

--- src/main/java/live/store/repositories/TransactionRepository.java
+++ src/main/java/live/store/repositories/TransactionRepository.java
@@ -1,5 +1,6 @@
 package live.store.repositories;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import live.store.entities.Transaction;
@@ -7,4 +8,6 @@ import reactor.core.publisher.Flux;
 
 public interface TransactionRepository extends ReactiveCrudRepository<Transaction, Long> {
 	Flux<Transaction> findByLiveSlug(String liveSlug);
+	@Query("SELECT * FROM transaction ORDER BY created_at DESC LIMIT 1;")
+	Flux<Transaction> getLastest();
 }
